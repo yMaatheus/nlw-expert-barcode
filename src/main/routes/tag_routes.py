f1d5870a5e5dc9bca2aa6abc076ad7a611a31bfd
Matheus <ymatheus.h@gmail.com>
2024-02-17 01:46:37 -0300

    feat: :sparkles: implements error handler

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 
+from src.errors.error_handler import handle_errors
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
 
@@ -8,9 +9,12 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 
 @tags_routes_bp.route('/create_tag', methods=['POST'])
 def create_tags():
-    tag_creator_view = TagCreatorView()
+    response = None
+    try:
+        tag_creator_view = TagCreatorView()
 
-    http_request = HttpRequest(body=request.json)
-    response = tag_creator_view.validate_and_create(http_request)
-
+        http_request = HttpRequest(body=request.json)
+        response = tag_creator_view.validate_and_create(http_request)
+    except Exception as exception:
+        response = handle_errors(exception)
     return jsonify(response.body), response.status_code
