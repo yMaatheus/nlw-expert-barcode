b4c3f72278d7b5a839e6bf8d775899873a11803e
Matheus <ymatheus.h@gmail.com>
2024-02-17 01:26:25 -0300

    feat: :sparkles: implements driver barcode handler

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,3 +1,4 @@
+from src.controllers.tag_creator_controller import TagCreatorController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
@@ -8,10 +9,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
 
-        print('estou na minha view')
-        print(http_request)
+        body = http_request.body
+        product_code = body["product_code"]
 
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200, body=formatted_response)
